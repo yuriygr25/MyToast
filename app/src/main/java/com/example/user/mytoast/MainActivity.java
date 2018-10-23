a55b5ceb930401a24cabe294a076f7a19acfad09
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-24 00:25:09 +0300

    commit

---
+++ app/src/main/java/com/example/user/mytoast/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.user.mytoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Click(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_layout,
+                (ViewGroup) findViewById(R.id.toast_layout));
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+
+        toast.setView(layout);
+        toast.show();
+    }
+}
